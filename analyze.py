1609802b9f92dd150fa8feb6a2b1955d84f6bc66
Bomani McClendon <bomanimcclendon2017@u.northwestern.edu>
2016-05-01 13:52:03 -0500

    Added some code to generate a correlation matrix for the dataframe

--- analyze.py
+++ analyze.py
@@ -3,3 +3,6 @@ import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 
 train = pd.read_csv("data/train.csv", parse_dates=['srch_ci', 'srch_co'], nrows=10000)
 train.info()
+
+corrMatrix = train.corr(method='pearson', min_periods=1)
+print(corrMatrix)
