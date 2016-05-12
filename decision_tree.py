2cd8e4ceb72e98e4c12a6469d8dd82e105223635
Bomani McClendon <bomanimcclendon2017@u.northwestern.edu>
2016-05-12 01:25:05 -0500

    Edited the script to train on a subset of the dataset, and then predict hotel clusters on a different subset as a way to measure method accuracy

--- decision_tree.py
+++ decision_tree.py
@@ -7,27 +7,6 @@ from sklearn.tree import DecisionTreeClassifier, export_graphviz
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 
-
-def encode_target(df, target_column):
-    """Add column to df with integers for the target.
-
-    Args
-    ----
-    df -- pandas DataFrame.
-    target_column -- column to map to int, producing
-                     new Target column.
-
-    Returns
-    -------
-    df_mod -- modified DataFrame.
-    targets -- list of target names.
-    """
-    df_mod = df.copy()
-    targets = df_mod[target_column].unique()
-    map_to_int = {name: n for n, name in enumerate(targets)}
-    df_mod["Target"] = df_mod[target_column].replace(map_to_int)
-    return (df_mod, targets)
-
 def visualize_tree(tree, feature_names):
     """Create tree png using graphviz.
 
@@ -36,29 +15,42 @@ def visualize_tree(tree, feature_names):
     tree -- scikit-learn DecsisionTree.
     feature_names -- list of feature names.
     """
-    with open("dt.dot", 'w') as f:
+    with open("output/dt.dot", 'w') as f:
         export_graphviz(tree, out_file=f,
                         feature_names=feature_names)
 
-    command = ["dot", "-Tpng", "dt.dot", "-o", "dt.png"]
+    command = ["dot", "-Tpng", "output/dt.dot", "-o", "output/dt.png"]
     try:
         subprocess.check_call(command)
     except:
         exit("Could not run dot, ie graphviz, to "
              "produce visualization")
 
-train = pd.read_csv("data/train.csv", parse_dates=['srch_ci', 'srch_co'], nrows=1000)
-train.info()
+# Load in the testing and training datasets
+trainFull = pd.read_csv("data/train.csv", parse_dates=['srch_ci', 'srch_co'], nrows=10000)
 
-df2, targets = encode_target(train, "hotel_cluster")
-print(df2.head())
-print(targets)
+# Take subsets of the datasets for training and testing
+train = trainFull.sample(1000)
+test_set = trainFull.sample(1000)
 
-# features = df2.columns.values.tolist()
-features = ["site_name", "hotel_market", "hotel_cluster"]
-y = df2["Target"]
-X = df2[features]
-dt = DecisionTreeClassifier(min_samples_split=20, random_state=99)
+# Set a list of features to be considered in the tree
+features = ["site_name", "hotel_market"]
+
+# Create and fit a decision tree to the set of data in those features
+y = train["hotel_cluster"]
+X = train[features]
+dt = DecisionTreeClassifier(min_samples_split=20)
 dt.fit(X, y)
 
+# Make a visualization of the decision tree
 visualize_tree(dt, features)
+
+# Print the Unique Classes
+print("Unique Classes: {}".format(dt.classes_))
+
+# Score ability to predict the right hotel clust for a new subset
+testX = test_set[features]
+testy = test_set["hotel_cluster"]
+score = dt.score(testX, testy)
+
+print("Score is " + str(score))
