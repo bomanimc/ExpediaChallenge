bc7684043fd1acd581a592a401dec5e0fd9fea0b
Bomani McClendon <bomanimcclendon2017@u.northwestern.edu>
2016-06-04 14:29:13 -0500

    Played with the settings to optimize accuracy for the random forest approach

--- random_forest.py
+++ random_forest.py
@@ -9,11 +9,11 @@ import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 
 
 # Load in the testing and training datasets
-trainFull = pd.read_csv("data/train.csv", nrows=100000)
+trainFull = pd.read_csv("data/train.csv", nrows=1000000)
 
 # Take subsets of the datasets for training and testing
-train = trainFull[:80000].sample(10000)
-test_set = trainFull[:20000].sample(10000)
+train = trainFull[:800000].sample(100000)
+test_set = trainFull[:200000].sample(100000)
 
 # Set a list of features to be considered in the tree
 features = trainFull.columns.values.tolist()
@@ -26,11 +26,14 @@ print(features)
 # Create and fit a decision tree to the set of data in those features
 y = trainFull["hotel_cluster"] 
 X = trainFull[features]
-rf = RandomForestClassifier(n_estimators=10)
+rf = RandomForestClassifier(n_estimators=20, n_jobs=-1, max_features=None)
 rf.fit(X, y)
 
 # Print the Unique Classes
 print("Unique Classes: {}".format(rf.classes_))
+
+# Print feature importances
+print(rf.feature_importances_)
 
 # Score ability to predict the right hotel clust for a new subset
 testX = test_set[features]
